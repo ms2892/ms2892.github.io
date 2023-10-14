f1c80f22a6f92d94a9a123129bfdcc8b6df64014
ms2892 <ms2892@cam.ac.uk>
2023-10-14 03:29:51 +0100

    Fixed Background size issue for projects

--- src/single_page_application/components/projects/components/projectItem.js
+++ src/single_page_application/components/projects/components/projectItem.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../src/css/projects.css'
 import PropTypes from 'prop-types';
 
 function ProjectItem(props) {
@@ -9,7 +8,7 @@ function ProjectItem(props) {
         
         <div className='project'>
             <a href={props.projectLink}>
-                <div className='project-image' style={{background:'url('+props.projectImage+')'}}></div>
+                <div className='project-image' style={{background:'url('+props.projectImage+')', backgroundSize:'cover'}}></div>
                 <div className='project-body'>
                     <h2 className='project-title'>{props.projectTitle}</h2>
                     <p className='project-description'>{props.projectDescription}</p>
