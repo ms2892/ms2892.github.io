7c51c7bb416ac29634a72b006045522aa298422b
Mohd Sadiq <msadiq074@gmail.com>
2024-11-05 13:25:06 +0000

    adding puzzles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './single_page_application/home.js'; 
+import Poem from './single_page_application/merve_puzzles/poem.js'
 import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ExperienceDisplay from './single_page_application/components/experience/experiencedisplay';
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/experienceDisplay" element={<ExperienceDisplay />} />
+        <Route path="/puzzle_one" element={<Poem />} />
       </Routes>
     </Router>
   );
---
+++ src/single_page_application/merve_puzzles/poem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './src/poem.css';
+
+class Poem extends React.Component {
+    render() {
+        return (
+            <div className="puzzle-one">
+                <h1>Hint</h1>
+                <br />
+                <p>In moonlight nights, my heart beats for you</p>
+                <p>Waiting on nothing more than only you</p>
+                <p>A light so true, it shines for me and you</p>
+                <p>Tempted by fate, I come to here to you</p>
+                <p>Under the blue skies, to test out my luck on last time</p>
+                <span>- Mohd Sadiq</span>
+                <br/>
+                <br/>
+
+            </div>
+        );
+    }
+}
+
+export default Poem;
