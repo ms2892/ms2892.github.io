7ff8b6e9e5ab89786852f28947bf489eef80a219
Mohd Sadiq <msadiq074@gmail.com>
2024-12-24 07:50:37 +0000

    Updates

--- src/single_page_application/merve_puzzles/merve.js
+++ src/single_page_application/merve_puzzles/merve.js
@@ -6,19 +6,33 @@ function SongPuzzle(){
     return(
         <div>
             Congratulations on figuring out the key.
-            
-
-        </div>
-    )
-}
-
-function HintPuzzle(){
-    return (
-        <div>
-            <p>
-                
-            </p>
-
+            If everything went well then you must be on the ferry towards Eminonu/Beyoglu. This puzzle was a small throwback to your days in Cambridge. It was a different time that we shared.
+            I still look at the photos I had clicked of you :P. The way you smiled in it was completely different from how you smiled in any other photos of yours. You truly looked happy. You looked like you were completely in your element.<br></br>
+            <br></br>
+            <br></br>
+            <img src="11.jpg" width="30%"/>
+            <img src="2.jpg" width="30%"/>
+            <img src="3.jpg" width="30%"/><br></br>
+            <img src="4.jpg" width="30%"/>
+            <img src="5.jpg" width="30%"/>
+            <img src="7.jpg" width="30%"/><br></br>
+            <img src="6.jpg" width="90%"/><br></br>
+            <img src="8.jpg" width="30%"/>
+            <img src="9.jpg" width="30%"/>
+            <img src="10.jpg" width="30%"/><br></br>
+            <br></br>
+            <br></br>
+            {/* <Image /> */}
+            One look at these pictures, one look at you, and I knew I wanted to keep that smile safe. I wanted to protect that side of you that I saw. And I never stopped thinking about that time. I never stopped thinking about you. I constantly 
+            wished everyday, dreamed everyday that you were here. At times, when I randomly fell asleep on that couch and woke up, I wished that I could hear you walking down the hallway. I missed your presence a lot. 
+            <br></br>
+            <br></br>
+            And ever since that day, the only thing I strive for is to make sure you get what you truly deserved. You deserve all the best things that this world has to offer. And I really truly wish to see you succeed. I wish to be on this journey with you 
+            as you achieve all the things you have ever wanted. And if you ever feel low, or hurt I just want you to know that I will always be here for you. I will always be here to support you. I will always be here to listen to you. I will always be here to 
+            be your safe space. I will always be here for you Merve. And I will always cheer you on, your biggest fan!!
+            <br></br>
+            <br></br>
+            I just want to see you happy and at peace.
         </div>
     )
 }
@@ -29,22 +43,8 @@ function Merve(){
     const [key, setKey] = useState();
     let originalKey = "dear my all"
 
-    let decodedText = "";
-
     function handleKeyChange(event){
         setKey(event.target.value)
-    }
-
-    function caesarMerve(key){
-        if(key==null){
-            return ""
-        }
-
-        if(key && key.toLowerCase() === originalKey.toLowerCase()){
-            return decodedText
-        }
-
-        
     }
 
     function songMerve(key){
@@ -54,16 +54,6 @@ function Merve(){
 
         if (key.toLowerCase()==originalKey){
             return <SongPuzzle />
-        }
-    }
-
-    function hintMerve(key){
-        if (key==null){
-            return ""
-        }
-
-        if (key.toLowerCase() === "penhaligon"){
-            return <HintPuzzle />
         }
     }
 
@@ -99,9 +89,7 @@ function Merve(){
             <br></br>
             <br></br>
             <div className='cipher-output'>
-                {caesarMerve(key)}
                 {songMerve(key)}
-                {hintMerve(key)}
             </div>
             <br />
             <br />
